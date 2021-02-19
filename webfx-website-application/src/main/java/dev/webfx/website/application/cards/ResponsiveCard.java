f98c23ae9177a4b087de532c7d791de54e5abf8e
Bruno Salmon <dev.salmonb@gmail.com>
2021-02-19 21:26:34 +0000

    Added Responsive card

---
+++ webfx-website-application/src/main/java/dev/webfx/website/application/cards/ResponsiveCard.java
@@ -0,0 +1,153 @@
+package dev.webfx.website.application.cards;
+
+import dev.webfx.extras.webtext.controls.HtmlText;
+import dev.webfx.platform.shared.services.resource.ResourceService;
+import dev.webfx.website.application.WebSiteShared;
+import javafx.animation.KeyValue;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.SVGPath;
+import javafx.scene.text.Text;
+
+/**
+ * @author Bruno Salmon
+ */
+final class ResponsiveCard extends FlipCard {
+
+    private Pane desktopTabletMobilePane;
+    private SVGPath thumbUp;
+
+    ResponsiveCard() {
+        super("Responsive");
+    }
+
+    private static Pane createScreen(Node content, double scale) {
+        Rectangle contentClip = new Rectangle();
+        content.setClip(contentClip);
+        content.setScaleX(scale);
+        content.setScaleY(scale);
+        return new Pane(content) {
+            @Override
+            protected void layoutChildren() {
+                double width = getWidth(), height = getHeight();
+                double borderWidth = 0.06 * Math.min(width, height), borderHeight = borderWidth * (scale < 0.5 ? 4 : 1);
+                WebSiteShared.setRegionBackground(this, Color.gray(0.1), new CornerRadii(borderWidth));
+                double cw = (width - 2 * borderWidth) / scale, ch = (height - 2 * borderHeight) / scale, cx = borderWidth - cw * (1 - scale) / 2, cy = borderHeight -ch * (1 - scale) / 2;
+                layoutInArea(content, cx, cy, cw, ch, 0, HPos.CENTER, VPos.CENTER);
+                contentClip.setWidth(cw);
+                contentClip.setHeight(ch);
+            }
+        };
+    }
+
+    private static Pane createScreen(double scale) {
+        return createScreen(new ResponsiveShowCasePane(), scale);
+    }
+
+    @Override
+    String caption(int step) {
+        switch (step) {
+            case 1: return "Your responsive web design made easy and powerful with the JavaFX layout system.";
+            case 2: return "JavaFX can callback your code during the layout pass to ask you to position nodes. This is a fantastic feature! Unmatched by HTML & CSS!";
+            case 3: return "As opposed to CSS rules, your layout code has access to your application context, giving you all the visibility you need to easily write your responsive design.";
+            case 4: return "While the number of CSS rules and functions are limited, you are empowered with all the possibilities of a programmatic language in your layout code.";
+            case 5: return "As opposed to CSS rules such as @media which works only at the top level, your layout code will work whatever its level in the scene graph.";
+            case 6: return "We made this website responsive entirely based on this JavaFX feature, using it at many different levels of the scene graph. Easy and powerful.";
+            default: return null;
+        }
+    }
+
+    @Override
+    void prepareCardTransition(int step, CardTransition cardTransition) {
+        super.prepareCardTransition(step, cardTransition);
+        switch (step) {
+            case 1:
+                if (desktopTabletMobilePane == null) {
+                    Pane desktopScreen = createScreen(1), tabletScreen = createScreen(0.7), mobileScreen = createScreen(0.3);
+                    Pane thumbUpPane = new ScalePane(thumbUp = WebSiteShared.createThumbUp());
+                    thumbUp.setFill(Color.WHITE);
+                    thumbUp.setEffect(WebSiteShared.dropShadow);
+                    desktopTabletMobilePane = new Pane(desktopScreen, tabletScreen, mobileScreen, thumbUpPane) {
+                        @Override
+                        protected void layoutChildren() {
+                            double width = getWidth(), height = getHeight();
+                            double dx = 0, dy = 0.1 * height, dw = width, dh = 0.7 * height;
+                            double th = 0.5 * height, tw = th * 3 / 4, tx = 0.65 * width - tw / 2, ty = 0.7 * height - th / 2;
+                            double mh = 0.25 * height, mw = mh * 414 / 736 , mx = 0.25 * width - mw / 2, my = ty + th - mh;
+                            double uw = width / 3, uh = height / 3, ux = width / 2 - uw / 2, uy = height / 2 - uh / 2;
+                            layoutInArea(desktopScreen, dx, dy, dw, dh, 0, HPos.CENTER, VPos.CENTER);
+                            layoutInArea(tabletScreen,  tx, ty, tw, th, 0, HPos.CENTER, VPos.CENTER);
+                            layoutInArea(mobileScreen,  mx, my, mw, mh, 0, HPos.CENTER, VPos.CENTER);
+                            layoutInArea(thumbUpPane,   ux, uy, uw, uh, 0, HPos.CENTER, VPos.CENTER);
+                        }
+                    };
+                }
+                cardTransition.addKeyValue(new KeyValue(thumbUp.opacityProperty(), 0));
+                flipToNewContent(desktopTabletMobilePane);
+                break;
+            case 2:
+            case 3:
+                Pane screen = createScreen(1);
+                HtmlText codeText = new HtmlText();
+                if (step == 2)
+                    codeText.setText("<div style=\"background-color: #1d1d26; color: #c9c9d1; font-family: monospace; font-size: 9.8pt;\">\n" +
+                            "<span style=\"color: #676773;\">// A typical JavaFX container with custom layout code:<br></span>Pane container = <span style=\"color: #e0957b;\">new </span>Pane(node1<span style=\"color: #e0957b; font-weight: bold;\">, </span>node2<span style=\"color: #e0957b; font-weight: bold;\">, </span>node3<span style=\"color: #e0957b; font-weight: bold;\">, </span>..., nodeN) {<br><span style=\"color: #85a658;\">&nbsp; &nbsp; @Override </span><span style=\"color: #e0957b;\">protected void </span><span style=\"color: #c7a65d;\">layoutChildren</span>() {<br><span style=\"color: #676773;\">&nbsp; &nbsp; &nbsp; &nbsp; // Your layout code here. It will be automatically<br></span><span style=\"color: #676773;\">&nbsp; &nbsp; &nbsp; &nbsp; // called by JavaFX at appropriate times!<br></span>&nbsp; &nbsp; }<br>}<span style=\"color: #e0957b; font-weight: bold;\">;<br></span>\n" +
+                            "</div>");
+                else
+                    codeText.setText("<div style=\"background-color: #1d1d26; color: #c9c9d1; font-family: monospace; font-size: 9.8pt;\">\n" +
+                            "<span style=\"color: #676773;\">// Example of a simple responsive design implementation:<br></span>Pane container = <span style=\"color: #e0957b;\">new </span>Pane(node1<span style=\"color: #e0957b; font-weight: bold;\">, </span>node2<span style=\"color: #e0957b; font-weight: bold;\">, </span>node3<span style=\"color: #e0957b; font-weight: bold;\">, </span>..., nodeN) {<br><span style=\"color: #85a658;\">&nbsp; &nbsp; @Override </span><span style=\"color: #e0957b;\">protected void </span><span style=\"color: #c7a65d;\">layoutChildren</span>() {<br><span style=\"color: #676773;\">&nbsp; &nbsp; &nbsp; &nbsp; // Getting the dimensions of the container<br></span><span style=\"color: #e0957b;\">&nbsp; &nbsp; &nbsp; &nbsp; double </span>width = getWidth()<span style=\"color: #e0957b; font-weight: bold;\">, </span>height = getHeight()<span style=\"color: #e0957b; font-weight: bold;\">;<br></span><span style=\"color: #e0957b;\"><span style=\"background-color: #1d1d26; color: #c9c9d1; font-family: monospace; font-size: 9.8pt;\"><span style=\"color: #676773;\">&nbsp; &nbsp; &nbsp; &nbsp; // Your responsive rules can look like this:<br></span></span>&nbsp; &nbsp; &nbsp; &nbsp; double </span>x1<span style=\"color: #e0957b; font-weight: bold;\">, </span>y1<span style=\"color: #e0957b; font-weight: bold;\">, </span>h1<span style=\"color: #e0957b; font-weight: bold;\">, </span>w1<span style=\"color: #e0957b; font-weight: bold;\">, </span>x2<span style=\"color: #e0957b; font-weight: bold;\">, </span>y2<span style=\"color: #e0957b; font-weight: bold;\">,</span> etc...<span style=\"color: #e0957b; font-weight: bold;\">;<br></span><span style=\"background-color: #1d1d26; color: #c9c9d1; font-family: monospace; font-size: 9.8pt;\"><span style=\"color: #e0957b;\">&nbsp; &nbsp; &nbsp; &nbsp; if </span>(width &gt; <span style=\"color: #4dacf0;\">1024</span>) {<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; x1 = ...<span style=\"color: #e0957b; font-weight: bold;\">; </span>y1 = ...<span style=\"color: #e0957b; font-weight: bold;\">;</span> etc...<span style=\"color: #e0957b; font-weight: bold;\"><br></span>&nbsp; &nbsp; &nbsp; &nbsp; } <span style=\"color: #e0957b;\">else if </span>(width &gt; <span style=\"color: #4dacf0;\">800</span>) {...} <span style=\"color: #e0957b;\">else </span>etc...</span>\n" +
+                            "</div>\n" +
+                            "<div style=\"background-color: #1d1d26; color: #c9c9d1; font-family: monospace; font-size: 9.8pt;\">\n" +
+                            "<span style=\"color: #676773;\">&nbsp; &nbsp; &nbsp; &nbsp; // You have direct access to your nodes and<br></span><span style=\"color: #676773;\">&nbsp; &nbsp; &nbsp; &nbsp; // can finally position them all in one go:<br></span> &nbsp; &nbsp; &nbsp; &nbsp; layoutInArea(<span style=\"color: #93a6f5;\">node1</span><span style=\"color: #e0957b; font-weight: bold;\">, </span>x1<span style=\"color: #e0957b; font-weight: bold;\">, </span>y1<span style=\"color: #e0957b; font-weight: bold;\">, </span>w1<span style=\"color: #e0957b; font-weight: bold;\">, </span>h1<span style=\"color: #e0957b; font-weight: bold;\">, </span>...)<span style=\"color: #e0957b; font-weight: bold;\">;<br></span>&nbsp; &nbsp; &nbsp; &nbsp; layoutInArea(<span style=\"color: #93a6f5;\">node2</span><span style=\"color: #e0957b; font-weight: bold;\">, </span>x2<span style=\"color: #e0957b; font-weight: bold;\">, </span>y2<span style=\"color: #e0957b; font-weight: bold;\">, </span>w2<span style=\"color: #e0957b; font-weight: bold;\">, </span>h2<span style=\"color: #e0957b; font-weight: bold;\">, </span>...)<span style=\"color: #e0957b; font-weight: bold;\">;<br></span>&nbsp; &nbsp; &nbsp; &nbsp; layoutInArea(<span style=\"color: #93a6f5;\">node3</span><span style=\"color: #e0957b; font-weight: bold;\">, </span>x3<span style=\"color: #e0957b; font-weight: bold;\">, </span>y3<span style=\"color: #e0957b; font-weight: bold;\">, </span>w3<span style=\"color: #e0957b; font-weight: bold;\">, </span>h3<span style=\"color: #e0957b; font-weight: bold;\">, </span>...)<span style=\"color: #e0957b; font-weight: bold;\">;<br></span><span style=\"color: #676773;\">&nbsp; &nbsp; &nbsp; &nbsp; ...<br></span>&nbsp; &nbsp; &nbsp; &nbsp; layoutInArea(<span style=\"color: #93a6f5;\">nodeN</span><span style=\"color: #e0957b; font-weight: bold;\">, </span>xN<span style=\"color: #e0957b; font-weight: bold;\">, </span>yN<span style=\"color: #e0957b; font-weight: bold;\">, </span>wN<span style=\"color: #e0957b; font-weight: bold;\">, </span>hN<span style=\"color: #e0957b; font-weight: bold;\">, </span>...)<span style=\"color: #e0957b; font-weight: bold;\">;<br></span>&nbsp; &nbsp; }<br>}<span style=\"color: #e0957b; font-weight: bold;\">;<br></span>\n" +
+                            "</div>");
+                flipToNewContent(new Pane(screen, codeText) {
+                    @Override
+                    protected void layoutChildren() {
+                        double width = getWidth(), height = getHeight();
+                        double codeHeight = codeText.prefHeight(width);
+                        layoutInArea(screen, 0, 0, width, height - codeHeight - 0.03 * height, 0, HPos.CENTER, VPos.TOP);
+                        layoutInArea(codeText, 0, height - codeHeight, width, codeHeight, 0, HPos.LEFT, VPos.BOTTOM);
+                    }
+                });
+                break;
+            case 4:
+                VBox vBox = new VBox(
+                        WebSiteShared.setHtmlText(new HtmlText(), "See how the EnzoClocks demo calculates the optimal positions of the clocks using a circle packing algorithm."),
+                        new ScalePane(new ImageView(ResourceService.toUrl("/dev/webfx/website/application/demos/EnzoClocks.png", getClass()))),
+                        WebSiteShared.setHtmlText(new HtmlText(), "Something impossible to achieve with CSS.")
+                );
+                vBox.setAlignment(Pos.CENTER);
+                vBox.setSpacing(0.05 * flipPanel.getHeight());
+                flipToNewContent(vBox);
+                break;
+            case 5:
+                Text text = WebSiteShared.setUpText(new Text("Move the bars"), 40, true, false, true, true);
+                text.setFill(Color.BLACK);
+                text.setMouseTransparent(true);
+                StackPane stackPane = new StackPane(
+                        createScreen(ResponsiveShowCasePane.createSplitPanes(), 1),
+                        text
+                );
+                stackPane.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> {
+                    cardTransition.addKeyValue(new KeyValue(text.opacityProperty(), 0));
+                    cardTransition.run(true);
+                });
+                flipToNewContent(stackPane);
+                break;
+            case 6:
+                cardTransition.addKeyValue(new KeyValue(thumbUp.opacityProperty(), 1));
+                flipToNewContent(desktopTabletMobilePane);
+                break;
+        }
+    }
+}
---
+++ webfx-website-application/src/main/java/dev/webfx/website/application/cards/ResponsiveShowCasePane.java
@@ -0,0 +1,80 @@
+package dev.webfx.website.application.cards;
+
+import dev.webfx.website.application.WebSiteShared;
+import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
+import javafx.geometry.VPos;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+
+/**
+ * @author Bruno Salmon
+ */
+final class ResponsiveShowCasePane extends Pane {
+
+    private final Region sandRegion   = createColorRegion(Color.web("#f08a5d"));
+    private final Region pinkRegion   = createColorRegion(Color.web("#b83b5e"));
+    private final Region greenRegion  = createColorRegion(Color.web("#158467"));
+    private final Region orangeRegion = createColorRegion(Color.web("#ff5722"));
+    private final Region yellowRegion = createColorRegion(Color.ORANGE);
+
+    public ResponsiveShowCasePane() {
+        getChildren().setAll(sandRegion, pinkRegion, greenRegion, yellowRegion, orangeRegion);
+    }
+
+    @Override
+    protected void layoutChildren() {
+        // Getting the width and height of this container
+        double width = getWidth(), height = getHeight();
+        // Salmon region x, y, w, h => by default: left part of top
+        double sx = 0, sy = 0, sw = width / 2, sh = 50; // 50px height
+        // Pink region x, y, w, h => by default: right part of top
+        double px = sw, py = sy, pw = width - sw, ph = sh;
+        // But if the salmon region becomes too small, we stack the 2 regions vertically
+        if (sw < 50) { // Let's say 50px is the minimal salmon width
+            sw = pw = width; // Finally allocating whole width for both regions
+            px = 0; py = sy + sh; // And moving the pink region below the salmon region
+        }
+        double rh = height - py - ph; // Remaining height (under the pink region)
+        // Green region x, y, w, h => by default: upper part of remaining space
+        double gx = 0, gy = py + ph, gw = width, gh = rh / 2;
+        // Orange region x, y, w, h => by default: central part of remaining space (1/4 of surface)
+        double ow = Math.sqrt(rh * width) / 2, oh = ow, ox = width / 2 - ow / 2, oy = gy + rh / 2 - oh / 2;
+        // Yellow region x, y, w, h => by default: lower part of remaining space
+        double yx = gx, yy = gy + gh, yw = gw, yh = gh;
+        if (oy < py + ph) {
+            gw = ow = yw = width / 3;
+            gh = oh = yh = rh;
+            ox = gx + gw;
+            yx = ox + ow;
+            oy = yy = gy;
+        }
+        if (ow * oh < 60 * 60) {
+            gw = ow = yw = width;
+            gx = ox = yx = 0;
+            oh = 60 * 60 / ow;
+            oy = gy + gh; yy = oy + oh;
+        }
+        // Finally applying the layout with all the above calculations
+        layoutInArea(sandRegion,   sx, sy, sw, sh, 0, HPos.CENTER, VPos.CENTER);
+        layoutInArea(pinkRegion,   px, py, pw, ph, 0, HPos.CENTER, VPos.CENTER);
+        layoutInArea(greenRegion,  gx, gy, gw, gh, 0, HPos.CENTER, VPos.CENTER);
+        layoutInArea(orangeRegion, ox, oy, ow, oh, 0, HPos.CENTER, VPos.CENTER);
+        layoutInArea(yellowRegion, yx, yy, yw, yh, 0, HPos.CENTER, VPos.CENTER);
+    }
+
+    private static Region createColorRegion(Color color) {
+        return WebSiteShared.setRegionBackground(new Region(), color);
+    }
+
+    static SplitPane createSplitPanes() {
+        SplitPane horizontalSplitPane = new SplitPane(new ResponsiveShowCasePane(), new ResponsiveShowCasePane());
+        horizontalSplitPane.setBackground(null);
+        SplitPane verticalSplitPane = new SplitPane(new ResponsiveShowCasePane(), horizontalSplitPane);
+        verticalSplitPane.setOrientation(Orientation.VERTICAL);
+        verticalSplitPane.setBackground(null);
+        return verticalSplitPane;
+    }
+}
