47deb02ef99909dc2e5ec67d884bb4cedad7e8e5
Bruno Salmon <dev.salmonb@gmail.com>
2022-05-16 17:47:52 +0100

    Reactivated the responsive card in the preview

--- webfx-website-application/src/main/java/dev/webfx/website/application/cards/Card.java
+++ webfx-website-application/src/main/java/dev/webfx/website/application/cards/Card.java
@@ -28,7 +28,7 @@ public abstract class Card extends LayoutPane {
             new JavaFullStackCard(),
             new CrossPlatformCard(),
             new SustainableCard(),
-            //new ResponsiveCard(), // Removed as advised by Duke and Ivan
+            new ResponsiveCard(),
             new MagicalCard(),
     };
 
