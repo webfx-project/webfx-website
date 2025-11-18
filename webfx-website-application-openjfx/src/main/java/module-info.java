90e24022d6ba48b76e4c82372f8eb449a4f25032
Bruno Salmon <dev.salmonb@gmail.com>
2025-11-18 11:24:36 +0000

    Updated website (demos videos were not working)

--- webfx-website-application-openjfx/src/main/java/module-info.java
+++ webfx-website-application-openjfx/src/main/java/module-info.java
@@ -6,13 +6,15 @@ module webfx.website.application.openjfx {
     requires javafx.media;
     requires webfx.extras.webtext.peers.openjfx;
     requires webfx.kit.javafxgraphics.openjfx;
+    requires webfx.platform.ast.factory.generic;
     requires webfx.platform.boot.java;
     requires webfx.platform.console.java;
-    requires webfx.platform.os.java;
-    requires webfx.platform.resource.java;
-    requires webfx.platform.scheduler.java;
-    requires webfx.platform.shutdown.java;
-    requires webfx.platform.windowlocation.java;
+    requires webfx.platform.os.jre;
+    requires webfx.platform.resource.jre;
+    requires webfx.platform.scheduler.jre;
+    requires webfx.platform.shutdown.jre;
+    requires webfx.platform.windowhistory.jre;
+    requires webfx.platform.windowlocation.jre;
     requires webfx.website.application;
 
 }
